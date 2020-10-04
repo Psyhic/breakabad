e49bfc2b15ef705438c335ff6cde74fa56090e3f
Psyhic <sahilkumarrana18112000@gmail.com>
2020-10-04 16:02:51 +0530

    Updates

---
+++ precache-manifest.4b1ae944ecce0ea9c9192c7e856bfc05.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddc2a4a3f0ec40a3e9cd9fa75463227",
+    "url": "/breakabad/index.html"
+  },
+  {
+    "revision": "55cf54af959b4420bf29",
+    "url": "/breakabad/static/css/main.b8905e65.chunk.css"
+  },
+  {
+    "revision": "a042b864f27e02f22f54",
+    "url": "/breakabad/static/js/2.e3c16cc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breakabad/static/js/2.e3c16cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55cf54af959b4420bf29",
+    "url": "/breakabad/static/js/main.060dd95d.chunk.js"
+  },
+  {
+    "revision": "239abd8e36e0b0f57c58",
+    "url": "/breakabad/static/js/runtime-main.eff72792.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breakabad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breakabad/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breakabad/static/media/spinner.235b9b3e.gif"
+  }
+]);
